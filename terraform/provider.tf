7f52602dbe39392340120964bfe5d8e3d97764cf
pratyusha04 <159904680+Pratyu0906@users.noreply.github.com>
2026-03-08 15:46:44 +0530

    Add AWS EC2 instances for devops setup
    Added AWS EC2 instances for devops master and agent.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "devops_master" {
+  ami           = "ami-0ff91eb5c6fe7cc86"
+  key_name      = "prt"
+  instance_type = "t3.micro"
+  tags = { Name = "devops-master" }
+}
+
+resource "aws_instance" "devops_agent" {
+  ami           = "ami-0ff91eb5c6fe7cc86"
+  instance_type = "t3.micro"
+  key_name      = "prt"
+  tags = { Name = "devops-agent" }
 }
